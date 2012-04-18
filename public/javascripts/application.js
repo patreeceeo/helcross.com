743a0a8dfc54009968aeb836d5af3e67f2ef69f9
Patrick Canfield <patrickc56@comcast.net>
2012-04-18 00:26:49 -0400

    did a bit of refactoring.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -176,77 +176,9 @@ $(function() {
 });
 
 function fbGetForum(name, callback) {
-  if(appMode === "development") {
-    log("returning dummy data.");
-    callback ({ // dummy data
-      "data": [{
-        "id": "175117605866523_374829559228659", 
-        "from": {
-            "name": "Kristian Gore", 
-            "id": "100000397654007"
-        }, 
-        "to": {
-            "data": [
-                {
-                  "version": 1, 
-                  "name": "Hel +", 
-                  "id": "175117605866523"
-                }
-              ]
-        }, 
-        "picture": "/content/images/me.jpg", 
-        "link": "#", 
-        "source": "#", 
-        "name": "Point w/ Steve Oh: F*D Justice System Episode", 
-        "caption": "www.youtube.com", 
-        "description": "Should the United States end the death penalty? How many false convictions come from eyewitness testimony, police lineups, and even DNA evidence? When will w...", 
-        "icon": "http://static.ak.fbcdn.net/rsrc.php/v1/yj/r/v2OnaTyTQZE.gif", 
-        "type": "video", 
-        "created_time": "2012-04-16T13:29:39+0000", 
-        "updated_time": "2012-04-16T13:29:39+0000", 
-        "comments": {
-            "count": 0
-        }
-      },
-      {
-        "id": "175117605866523_374829559228659", 
-        "from": {
-            "name": "Kristian Gore", 
-            "id": "100000397654007"
-        }, 
-        "to": {
-            "data": [
-                {
-                  "version": 1, 
-                  "name": "Hel +", 
-                  "id": "175117605866523"
-                }
-              ]
-        }, 
-        "picture": "/content/images/me.jpg", 
-        "link": "#", 
-        "source": "#", 
-        "name": "Point w/ Steve Oh: F*D Justice System Episode", 
-        "caption": "www.youtube.com", 
-        "description": "The next one.", 
-        "icon": "http://static.ak.fbcdn.net/rsrc.php/v1/yj/r/v2OnaTyTQZE.gif", 
-        "type": "video", 
-        "created_time": "2012-04-16T13:29:39+0000", 
-        "updated_time": "2012-04-16T13:29:39+0000", 
-        "comments": {
-            "count": 0
-          }
-        }],
-      "paging": {
-        "previous": "#", 
-        "next": "#"
-      } 
-    });
-  } else { // if app mode is not development
-    FB.api("/175117605866523/feed?limit=10&access_token=368471183197357|eCJFFVks1PDmkjSI-g7G9QqGe5w", function(r) {
-      callback(r);
-    });
-  }
+  FB.api("/175117605866523/feed?limit=10&access_token=368471183197357|eCJFFVks1PDmkjSI-g7G9QqGe5w", function(r) {
+    callback(r);
+  });
 }
 
 
