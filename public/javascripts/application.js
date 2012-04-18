ddfc6cec16e516fd08b5224c3d662f4d319bd571
Patrick Canfield <patrickc56@comcast.net>
2012-04-17 23:40:55 -0400

    did a bit of refactoring.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,5 +1,5 @@
 // var appMode = "development";
-var appMode = "";
+// var appMode = "";
 var fbUser = null;
 
 function log(message) {
@@ -29,16 +29,33 @@ $(document).ready(function() {
   });
 
   
-  if(appMode == "development") {
-    appHandleEvent("login");
-    showPage("cross-you");
-  } else {
-    appHandleEvent("logout");
-    showPage("home");
-  }
+  // if(appMode == "development") {
+  //   appHandleEvent("login");
+  //   showPage("cross-you");
+  // } else {
+  //   appHandleEvent("logout");
+  //   showPage("home");
+  // }
 
 });
 
+
+function fbHandleEvent (response) {
+  log("fbHandleEvent");
+  if (response.authResponse) {
+    // user has auth'd your app and is logged into Facebook
+    FB.api ('/me', function (me) {
+      fbUser = me;
+    });
+    log("fbUser: ", fbUser);
+    appHandleEvent("login");
+  } else {
+    // if(!response.session) {
+    appHandleEvent("logout");
+    // }
+    // FB.logout(fbHandleEvent);
+  }
+}
 
 function appHandleEvent(e) {
   if (e == "login") {
@@ -151,4 +168,149 @@ function appRenderForum(stor) {
     };
   $(stor).append(t(forum));
     // });
+
+}
+
+
+
+function logResponse(response) {
+  if (console && console.log) {
+    console.log('The response was', response);
+  }
+}
+
+$(function() {
+  // Set up so we handle click on the buttons
+  var dataUrl = this.location.href;
+  $('#post-to-wall').click(function() {
+    FB.ui ({
+      method : 'feed',
+      link   : dataUrl
+      },
+    function (response) {
+      // If response is null the user canceled the dialog
+      if (response != null) {
+        logResponse(response);
+      }
+    }
+    );
+  });
+
+  $('#send-to-friends').click(function() {
+    FB.ui(
+      {
+      method : 'send',
+      link   : dataUrl
+    },
+    function (response) {
+      // If response is null the user canceled the dialog
+      if (response != null) {
+        logResponse(response);
+      }
+    }
+    );
+  });
+
+  $('#app-request').click(function() {
+    FB.ui(
+      {
+      method  : 'apprequests',
+      message : dataUrl
+    },
+    function (response) {
+      // If response is null the user canceled the dialog
+      if (response != null) {
+        logResponse(response);
+      }
+    }
+    );
+  });
+
+  $('#add-page-tab').click(function() {
+    log("you clicked #add-page-tab");
+    FB.ui({
+      method: 'pagetab',
+      redirect_uri: dataUrl
+    },
+    function (response) {
+      // If response is null the user canceled the dialog
+      if (response != null) {
+        logResponse(response);
+      }
+    }); 
+  });
+});
+
+function fbGetForum(name, callback) {
+  if(appMode === "development") {
+    log("returning dummy data.");
+    callback ({ // dummy data
+      "data": [{
+        "id": "175117605866523_374829559228659", 
+        "from": {
+            "name": "Kristian Gore", 
+            "id": "100000397654007"
+        }, 
+        "to": {
+            "data": [
+                {
+                  "version": 1, 
+                  "name": "Hel +", 
+                  "id": "175117605866523"
+                }
+              ]
+        }, 
+        "picture": "/content/images/me.jpg", 
+        "link": "#", 
+        "source": "#", 
+        "name": "Point w/ Steve Oh: F*D Justice System Episode", 
+        "caption": "www.youtube.com", 
+        "description": "Should the United States end the death penalty? How many false convictions come from eyewitness testimony, police lineups, and even DNA evidence? When will w...", 
+        "icon": "http://static.ak.fbcdn.net/rsrc.php/v1/yj/r/v2OnaTyTQZE.gif", 
+        "type": "video", 
+        "created_time": "2012-04-16T13:29:39+0000", 
+        "updated_time": "2012-04-16T13:29:39+0000", 
+        "comments": {
+            "count": 0
+        }
+      },
+      {
+        "id": "175117605866523_374829559228659", 
+        "from": {
+            "name": "Kristian Gore", 
+            "id": "100000397654007"
+        }, 
+        "to": {
+            "data": [
+                {
+                  "version": 1, 
+                  "name": "Hel +", 
+                  "id": "175117605866523"
+                }
+              ]
+        }, 
+        "picture": "/content/images/me.jpg", 
+        "link": "#", 
+        "source": "#", 
+        "name": "Point w/ Steve Oh: F*D Justice System Episode", 
+        "caption": "www.youtube.com", 
+        "description": "The next one.", 
+        "icon": "http://static.ak.fbcdn.net/rsrc.php/v1/yj/r/v2OnaTyTQZE.gif", 
+        "type": "video", 
+        "created_time": "2012-04-16T13:29:39+0000", 
+        "updated_time": "2012-04-16T13:29:39+0000", 
+        "comments": {
+            "count": 0
+          }
+        }],
+      "paging": {
+        "previous": "#", 
+        "next": "#"
+      } 
+    });
+  } else { // if app mode is not development
+    FB.api("/175117605866523/feed?limit=10&access_token=368471183197357|eCJFFVks1PDmkjSI-g7G9QqGe5w", function(r) {
+      callback(r);
+    });
+  }
 }
--- public/javascripts/lib/fb-init.js
+++ public/javascripts/lib/fb-init.js
@@ -1,19 +1,3 @@
-function fbHandleEvent (response) {
-  log("fbHandleEvent");
-  if (response.authResponse) {
-    // user has auth'd your app and is logged into Facebook
-    FB.api ('/me', function (me) {
-      fbUser = me;
-    });
-  log("fbUser: ", fbUser);
-  appHandleEvent("login");
-  } else {
-    // if(!response.session) {
-    appHandleEvent("logout");
-    // }
-    // FB.logout(fbHandleEvent);
-  }
-}
 
 (function(d) {
   var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
@@ -44,17 +28,10 @@ window.fbAsyncInit = function() {
   
   // respond to clicks on the login and logout links
   $('#auth-loginlink').click(function(e) {
-    if(appMode == "development") {
-      appHandleEvent("login");
-    } else
-      FB.login();
+  FB.login();
   });
   $('#auth-logoutlink').click(function(e) {
-    if(appMode == "development")
-      appHandleEvent("logout");
-    else
-      FB.logout();
+  FB.logout();
   }); 
-
 }
 
